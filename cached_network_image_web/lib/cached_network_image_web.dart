86ffc7c07de648d61d32f84c0951623838d850fa
LulzM <yunmo159789@gmail.com>
2024-04-26 16:34:51 +0900

    Fixed webOnlyInstantiateImageCodecFromUrl to createImageCodecFromUrl

--- cached_network_image_web/lib/cached_network_image_web.dart
+++ cached_network_image_web/lib/cached_network_image_web.dart
@@ -4,10 +4,10 @@ library cached_network_image_web;
 import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
+import 'dart:ui_web';
 
 import 'package:cached_network_image_platform_interface'
-        '/cached_network_image_platform_interface.dart' as platform
-    show ImageLoader, ImageRenderMethodForWeb;
+    '/cached_network_image_platform_interface.dart' as platform show ImageLoader, ImageRenderMethodForWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
@@ -155,7 +155,8 @@ class ImageLoader implements platform.ImageLoader {
   ) {
     final resolved = Uri.base.resolve(url);
     // ignore: undefined_function
-    return ui.webOnlyInstantiateImageCodecFromUrl(
+    // return ui.webOnlyInstantiateImageCodecFromUrl(
+    return createImageCodecFromUrl(
       resolved,
       chunkCallback: (int bytes, int total) {
         chunkEvents.add(
@@ -165,7 +166,7 @@ class ImageLoader implements platform.ImageLoader {
           ),
         );
       },
-    ) as Future<ui.Codec>;
+    );
   }
 }
 
